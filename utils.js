091d5f1293b20f9bcc066641534be6b20167c8d5
venkatesh k <66763666+venkylad@users.noreply.github.com>
2023-07-12 22:33:02 +0530

    commit

---
+++ utils.js
@@ -0,0 +1,4 @@
+export const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+export const phoneRegex = /^(?=(\D*\d){0,10}\D*$).*/;
+export const vehicleRegistrationRegex = /^[A-Z]+$/;
+export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
